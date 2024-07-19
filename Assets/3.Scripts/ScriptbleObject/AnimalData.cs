e5d0c35cdd5c203956398bdc8f88749f490f2ac7
SinBe <kimtjwnsyba@gmail.com>
2024-07-19 16:58:34 +0900

    Add AnimalData

---
+++ Assets/3.Scripts/ScriptbleObject/AnimalData.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "AnimalData", menuName = "ScriptbleObject/AnimalData", order = int.MaxValue)]
+public class AnimalData : ScriptableObject
+{
+    [SerializeField] private string animalName;
+    public string AnimalName {  get { return animalName; } }
+
+    [SerializeField] private float maxHp;
+    public float MaxHP { get { return maxHp; } }
+
+    [SerializeField] private float damage;
+    public float Damage { get { return damage; } }
+
+    [SerializeField] private GameObject rewardItem;
+    public GameObject RewardItem { get { return rewardItem; } }
+}
