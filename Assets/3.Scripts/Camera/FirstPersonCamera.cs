7dfb3954502689c96ec8e1d5cc967e4146fff807
JunghyunByun <bjh1206408@gmail.com>
2024-07-20 10:06:38 +0900

    PlayerController & FirstPersonCamera Update

---
+++ Assets/3.Scripts/Camera/FirstPersonCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float mouseSensitivity = 2f;
+    private float cameraVerticalRotation = 0f;
+    private Vector2 lookInput;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        CameraLook();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    void CameraLook()
+    {
+        float inputX = lookInput.x * mouseSensitivity;
+        float inputY = lookInput.y * mouseSensitivity;
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+
+        player.Rotate(Vector3.up * inputX);
+    }
+}
--- Assets/3.Scripts/Player/PlayerController.cs
+++ Assets/3.Scripts/Player/PlayerController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void Run()
+    {
+        Vector3 velocity = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.y * speed);
+        rb.velocity = transform.TransformDirection(velocity);
     }
 }
