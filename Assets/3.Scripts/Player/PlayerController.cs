c24241da5727f164637b2f34fab2cc72811ca25a
JunghyunByun <bjh1206408@gmail.com>
2024-07-22 00:02:56 +0900

    Add Player Run Animation

--- Assets/3.Scripts/Player/PlayerController.cs
+++ Assets/3.Scripts/Player/PlayerController.cs
@@ -7,13 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody rb;
-    private Animator animator;
+    private Animator anim;
     private Vector3 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,5 +30,14 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 velocity = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.y * speed);
         rb.velocity = transform.TransformDirection(velocity);
+
+        if (moveInput.x != 0 || moveInput.y != 0)
+        {
+            anim.Play("Run");
+        }
+        else
+        {
+            anim.Play("Idle");
+        }
     }
 }
