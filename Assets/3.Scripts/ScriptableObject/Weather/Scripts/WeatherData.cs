ea6db58c822cb4e8c3a179aa7c9ec3dfb15ea278
SinBe <kimtjwnsyba@gmail.com>
2024-08-01 22:56:00 +0900

    Add asset & Edit Code

--- Assets/3.Scripts/ScriptableObject/Weather/Scripts/WeatherData.cs
+++ Assets/3.Scripts/ScriptableObject/Weather/Scripts/WeatherData.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeatherData", menuName = "ScriptableObject/WeatherData", order = int.MaxValue)]
 public class WeatherData : ScriptableObject
 {
-    [SerializeField] private GameObject weatherPre;
-    public GameObject WeatherPre {  get { return weatherPre; } }
+    [SerializeField] private GameObject weatherObject;
+    public GameObject WeatherObject { get { return weatherObject; } }
 
     [SerializeField] private int weatherDate;
     public int WeatherDate { get { return weatherDate; } }
