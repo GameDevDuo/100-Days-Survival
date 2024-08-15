c41c76e6096f2f2c6bf7dbc033ad6cfee9c7d765
JunghyunByun <bjh1206408@gmail.com>
2024-08-15 11:49:11 +0900

    Add BuildHandler 🛠️

---
+++ Assets/3.Scripts/Inventory/BuildHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
