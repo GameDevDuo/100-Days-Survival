3479797e21dab091ebcdbfaa2950ae1e87a36731
SinBe <kimtjwnsyba@gmail.com>
2024-09-06 18:39:39 +0900

    Add Tornado Code

---
+++ Assets/3.Scripts/Static/BezierCurve.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class BezierCurve
+{
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float ttt = tt * t;
+        float uuu = uu * u;
+
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+
+        return p;
+    }
+}
