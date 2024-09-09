be2cd7eb9079460c1b2f59505ea4f4f2718166b2
SinBe <kimtjwnsyba@gmail.com>
2024-09-09 20:31:24 +0900

    Edit Volcano Code

--- Assets/3.Scripts/ScriptableObject/Weather/Scripts/Volcano.cs
+++ Assets/3.Scripts/ScriptableObject/Weather/Scripts/Volcano.cs
@@ -8,13 +8,6 @@ public class Volcano : RandomPosBase
     private List<GameObject> meteor = new List<GameObject>();
 
     [SerializeField] Transform spawnPos;
-
-    private Vector3 target;
-
-    private float smallAngle = 60f;
-    private float largeAngle = 120f;
-    private float time;
-
     private int maxCount;
 
     private void Awake()
@@ -39,43 +32,14 @@ public class Volcano : RandomPosBase
 
     private void SpawnMeteor()
     {
-        target = GetRandomPointInRange();
         GameObject selectedMeteor = meteor[Random.Range(0, meteor.Count)];
-        Rigidbody rb = selectedMeteor.GetComponent<Rigidbody>();
 
-        Instantiate(selectedMeteor, spawnPos.position, Quaternion.identity);
-        
-        float randomAngle = Random.Range(smallAngle, largeAngle);
-        Vector3 velocity = GetVelocity(spawnPos.position, target, randomAngle);
+        float randomX = Random.Range(-80f, -30f);
+        float randomY = Random.Range(-180f, 180f);
+        Quaternion quaternion = new Quaternion(randomX, randomY, 0f, 0f);
+        GameObject meteorInstance =  Instantiate(selectedMeteor, spawnPos.position, quaternion);
 
-        rb.velocity = velocity;
-    }
-
-    private Vector3 GetVelocity(Vector3 start, Vector3 target, float initialAngle)
-    {
-        float gravity = Physics.gravity.magnitude;
-        float angle = initialAngle * Mathf.Deg2Rad;
-
-        Vector3 planarTarget = new Vector3(target.x, 0f, target.z);
-        Vector3 planarPos = new Vector3(start.x, 0f, start.z);
-
-        float distance = Vector3.Distance(planarTarget, planarPos);
-        float yOffset = start.y - target.y;
-
-        float initialVelocity = (1 / Mathf.Cos(angle)) *
-            Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) /
-            (distance * Mathf.Tan(angle) + yOffset));
-
-        Vector3 velocity = new Vector3(0f,
-            initialVelocity * Mathf.Sin(angle),
-            initialVelocity * Mathf.Cos(angle));
-
-        float angleBetweenObject = Vector3.Angle(Vector3.forward,
-            planarTarget - planarPos) * (target.x > start.x ? 1 : -1);
-
-        Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObject,
-            Vector3.up) * velocity;
-
-        return finalVelocity;
+        Rigidbody rb = meteorInstance.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * Random.Range(70f, 100f), ForceMode.Impulse);
     }
 }
