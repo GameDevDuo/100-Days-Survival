97f0acdfb773b6b928f9f581ffd5ea02ccdb8ea1
JunghyunByun <bjh1206408@gmail.com>
2024-09-12 23:10:18 +0900

    AnimalData Update

--- Assets/3.Scripts/ScriptableObject/Animal/Scripts/AnimalData.cs
+++ Assets/3.Scripts/ScriptableObject/Animal/Scripts/AnimalData.cs
@@ -5,16 +5,12 @@ public class AnimalData : ScriptableObject
 {
     [SerializeField] private string animalName;
     public string AnimalName {  get { return animalName; } }
-
     [SerializeField] private float maxHp;
     public float MaxHP { get { return maxHp; } }
-
     [SerializeField] private float damage;
     public float Damage { get { return damage; } }
-
     [SerializeField] private float attakDistance;
     public float AttackDistance { get { return attakDistance; } }
-
     [SerializeField] private float findRange;
     public float FindRange { get {  return findRange; } }
 }
